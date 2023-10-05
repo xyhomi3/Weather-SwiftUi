92aba2168a1d327f5ae489926a7f0e6d766e828c
XYHub <lucien.loua@gmail.com>
2023-10-05 13:10:28 +0000

    Initial Commit

---
+++ Weather-SwiftUi/Weather_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUiApp.swift
+//  Weather-SwiftUi
+//
+//  Created by HOMi3Verse on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
