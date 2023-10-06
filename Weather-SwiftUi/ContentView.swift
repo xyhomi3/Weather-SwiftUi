d53612df29cb49d477cc3f170d9abf3001e31652
XYHub <lucien.loua@gmail.com>
2023-10-06 02:16:29 +0000

    add: weather

--- Weather-SwiftUi/ContentView.swift
+++ Weather-SwiftUi/ContentView.swift
@@ -8,28 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
-            VStack(spacing: 15){
-                Text("Dakar, SN")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack{
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 150, height: 150)
-                    Text("30°")
-                        .font(.system(size: 70, weight: .medium, design: .default))
-                        .foregroundColor(.white)
-                }
-                Spacer()
+            BackgroundView(isNight: $isNight)
+            VStack{
+                CityTextView(cityName: "Dakar, SN")
+                
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill",
+                                      temperature: 30)
                 
                 HStack(spacing: 20){
                     WeatheDayView(dayOfWeek: "TUE",
@@ -51,6 +40,16 @@ struct ContentView: View {
                     WeatheDayView(dayOfWeek: "SAT",
                                   imageName: "sunset.fill",
                                   temperature: 26)
+                }
+                    
+                Spacer()
+                
+                Button{
+                    isNight.toggle()
+                } label:{
+                    WeatherButton(title: "Change Day Time",
+                                  textColor: .blue,
+                                  backGroundColor: .white)
                 }
                 Spacer()
             }
@@ -86,3 +85,60 @@ struct WeatheDayView: View {
         }
     }
 }
+
+struct BackgroundView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View{
+        VStack{
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180 )
+            Text("\(temperature)°")
+                .font(.system(size: 81, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backGroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backGroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
+
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by XY HOMi3 on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
